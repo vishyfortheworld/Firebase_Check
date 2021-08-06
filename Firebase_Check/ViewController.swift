9f87d33ee945f5aa3896ec08a196ecbc92ef15b6
Vishrut Vatsa <vishrut.official@gmail.com>
2021-08-06 15:23:38 +0530

    Initial Commit

---
+++ Firebase_Check/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_Check
+//
+//  Created by Vishrut Vatsa on 06/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
